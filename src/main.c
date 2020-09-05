625edf2749290efbb476791d5dd66eac51d1414d
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-09-05 17:25:46 +0200

    implements lab8

--- src/main.c
+++ src/main.c
@@ -10,38 +10,55 @@ void Delay1ms(unsigned long msec){
     }
 }
 
+#define PE0   (*((volatile unsigned long *)0x40024004))
+void Switch_Init(void){ volatile unsigned long delay;
+    SYSCTL_RCGC2_R |= 0x00000010;
+    delay = SYSCTL_RCGC2_R;
+
+    GPIO_PORTE_AMSEL_R &= ~0x01;
+    GPIO_PORTE_PCTL_R &= ~0x0000000F;
+    GPIO_PORTE_DIR_R &= ~0x01;
+    GPIO_PORTE_AFSEL_R &= ~0x01;
+    GPIO_PORTE_DEN_R |= 0x01;
+}
+
+unsigned long Switch_Input(void){
+    return PE0;
+}
+
+#define PE1   (*((volatile unsigned long *)0x40024008))
 void LED_Init(void){
     volatile unsigned long delay;
-    SYSCTL_RCGC2_R |= 0x01;           // 1) activate clock for Port A
-    delay = SYSCTL_RCGC2_R;           // allow time for clock to start
-                                    // 2) no need to unlock PA2
-    GPIO_PORTA_PCTL_R &= ~0x00000F00; // 3) regular GPIO
-    GPIO_PORTA_AMSEL_R &= ~0x04;      // 4) disable analog function on PA2
-    GPIO_PORTA_DIR_R |= 0x04;         // 5) set direction to output
-    GPIO_PORTA_AFSEL_R &= ~0x04;      // 6) regular port function
-    GPIO_PORTA_DEN_R |= 0x04;         // 7) enable digital port
+    SYSCTL_RCGC2_R |= 0x10;
+    delay = SYSCTL_RCGC2_R;
+
+    GPIO_PORTE_PCTL_R &= ~0x000000F0;
+    GPIO_PORTE_AMSEL_R &= ~0x02;
+    GPIO_PORTE_DIR_R |= 0x02;
+    GPIO_PORTE_AFSEL_R &= ~0x02;
+    GPIO_PORTE_DEN_R |= 0x02;
 }
 
-// Make PA2 high
 void LED_On(void){
-    GPIO_PORTA_DATA_R |= 0x04;
+    PE1 |= 0x02;
 }
 
-// Make PA2 low
 void LED_Off(void){
-    GPIO_PORTA_DATA_R &= ~0x04;
+    PE1 &= ~0x02;
 }
 
 int main(void) {
     PortF_Init();
+    Switch_Init();
     LED_Init();
 
     while (1) {
-        if (PF4 == 0) {
-            LED_On();
-        } else {
+        LED_On();
+        Delay1ms(100);
+        if (Switch_Input()) {
             LED_Off();
         }
+        Delay1ms(100);
     }
 }
 
